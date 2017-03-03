693dce08d2c2adc7e9e0d6f3ea9cbfc92a4071ab
Sokmean <sokmean.nou@gmail.com>
2017-03-03 11:01:17 -0500

    Update PriceQtyAddtoCart.jsx

--- app/components/PriceQtyAddtoCart.jsx
+++ app/components/PriceQtyAddtoCart.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const PriceQtyAddtoCart = () => (
   <div>
-    <h4>Reviews</h4>
-      <h4>Price: $SomePrice</h4>
-      <input id="qtyselector" type="number" min="1" />
-      <button id="addtocartbtn"><i className="fa fa-shopping-cart" /> Add to cart </button>
+    <h4>Price: $SomePrice</h4>
+    <input id="qtyselector" type="number" min="1" />
+    <button id="addtocartbtn"><i className="fa fa-shopping-cart" /> Add to cart </button>
   </div>
 )
 
